2f1583bc46aa7de58bf357d7d0b6c844e7dfdd44
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-11-27 21:04:53 +0100

    Add a compiler from pip

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-link_sycl() {
-    mkdir -p $HOME/miniforge3/envs/triton/$1
-    ln -snf /opt/intel/oneapi/compiler/latest/include/sycl $HOME/miniforge3/envs/triton/$1/
-    ln -snf /opt/intel/oneapi/compiler/latest/include/sycl/CL $HOME/miniforge3/envs/triton/$1/
-}
-
 install_env() {
     export PATH="$HOME/miniforge3/bin:$PATH"
     conda create -n triton --override-channels -c conda-forge python=$python_version.*
@@ -13,8 +7,6 @@ install_env() {
     find /opt/intel/oneapi/ \( -name '*.so' -or -name '*.so.*' \) -exec cp -n {} $HOME/miniforge3/envs/triton/lib \;
     ln -snf /usr/include/level_zero $HOME/miniforge3/envs/triton/bin/../x86_64-conda-linux-gnu/sysroot/usr/include/level_zero
     find /usr -name libze_\* -exec cp -n {} $HOME/miniforge3/envs/triton/lib \;
-    link_sycl lib/python$python_version/site-packages/triton/backends/intel/include
-    link_sycl x86_64-conda-linux-gnu/sysroot/usr/include
 }
 
 print_env_info() {
