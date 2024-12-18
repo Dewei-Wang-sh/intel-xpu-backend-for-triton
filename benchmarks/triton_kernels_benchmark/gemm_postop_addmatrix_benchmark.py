7a66ce76596a733d4c6dfcbe847ee9e1aebb59ea
Alex Baden <alexander.baden@intel.com>
2024-12-18 01:26:41 +0000

    add int8 to addmatrix benchmark 3/3

--- benchmarks/triton_kernels_benchmark/gemm_postop_addmatrix_benchmark.py
+++ benchmarks/triton_kernels_benchmark/gemm_postop_addmatrix_benchmark.py
@@ -121,8 +121,7 @@ def matmul_kernel_with_block_pointers_batched(
         stride_az: tl.constexpr, stride_am: tl.constexpr, stride_ak: tl.constexpr,  #
         stride_bz: tl.constexpr, stride_bk: tl.constexpr, stride_bn: tl.constexpr,  #
         stride_cz: tl.constexpr, stride_cm: tl.constexpr, stride_cn: tl.constexpr,  #
-        stride_dz: tl.constexpr, stride_dm: tl.constexpr, stride_dn: tl.constexpr,
-        ACCUMULATOR_DTYPE: tl.constexpr,
+        stride_dz: tl.constexpr, stride_dm: tl.constexpr, stride_dn: tl.constexpr, ACCUMULATOR_DTYPE: tl.constexpr,
         # Meta-parameters
         BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr, GROUP_SIZE_M: tl.constexpr):
     bid = tl.program_id(axis=0)
@@ -218,29 +217,31 @@ def matmul(a, b, d, c):
         # argument names to use as an x-axis for the plot
         x_names=['B', 'M', 'K', 'N', 'dtype'],
         # different possible values for `x_name`
-        x_vals=[[1, 1024 * i, 1024 * i, 1024 * i, dtype] for i in [1, 2, 4, 8] for dtype in [torch.bfloat16]] +  #
-        [[*shape, dtype] for shape in [
-            [1, 1, 5120, 13824],  #
-            [1, 4, 4096, 12288],  #
-            [1, 512, 8192, 8192],  #
-            [1, 512, 8192, 32768],  #
-            [1, 512, 32768, 8192],  #
-            [1, 1024, 16384, 8192],  #
-            [1, 1024, 28672, 8192],  #
-            [1, 3072, 4096, 3072],  # FIXME: Remove this case when gemm_streamk_benchmark works
-            [1, 4096, 16384, 8192],  #
-            [1, 8192, 16384, 1024],  #
-            [1, 8192, 16384, 4096],  #
-            [1, 16384, 1024, 8192],  #
-            [1, 16384, 4096, 8192],  #
-            [1, 16384, 8192, 1024],  #
-            [1, 16384, 8192, 4096],  #
-            [4, 32768, 128, 4096],  #
-            [4, 32768, 4096, 128],  #
-            [32, 4096, 4096, 128],  #
-            [4096, 8, 128, 16384],  #
-            [4096, 8, 16384, 128]
-        ] for dtype in [torch.bfloat16]], 
+        x_vals=[[1, 1024 * i, 1024 * i, 1024 * i, dtype]
+                for i in [1, 2, 4, 8]
+                for dtype in [torch.bfloat16, torch.int8]] +  #
+        [[*shape, dtype]
+         for shape in [[1, 1, 5120, 13824],  #
+                       [1, 4, 4096, 12288],  #
+                       [1, 512, 8192, 8192],  #
+                       [1, 512, 8192, 32768],  #
+                       [1, 512, 32768, 8192],  #
+                       [1, 1024, 16384, 8192],  #
+                       [1, 1024, 28672, 8192],  #
+                       [1, 3072, 4096, 3072],  # FIXME: Remove this case when gemm_streamk_benchmark works
+                       [1, 4096, 16384, 8192],  #
+                       [1, 8192, 16384, 1024],  #
+                       [1, 8192, 16384, 4096],  #
+                       [1, 16384, 1024, 8192],  #
+                       [1, 16384, 4096, 8192],  #
+                       [1, 16384, 8192, 1024],  #
+                       [1, 16384, 8192, 4096],  #
+                       [4, 32768, 128, 4096],  #
+                       [4, 32768, 4096, 128],  #
+                       [32, 4096, 4096, 128],  #
+                       [4096, 8, 128, 16384],  #
+                       [4096, 8, 16384, 128]]
+         for dtype in [torch.bfloat16, torch.int8]],
         line_arg='provider',
         # argument name whose value corresponds to a different line in the plot
         # possible values for `line_arg``
@@ -257,9 +258,9 @@ def matmul(a, b, d, c):
 def benchmark(B, M, N, K, dtype, provider):
     res_dtype = torch.float32 if dtype is torch.bfloat16 else torch.int32
     if dtype.is_floating_point:
-        rand = lambda shape, dtype : torch.rand(shape, device='xpu', dtype=dtype)
+        rand = lambda shape, dtype: torch.rand(shape, device='xpu', dtype=dtype)
     else:
-        rand = lambda shape, dtype : torch.randint(low=-127, high=128, size=shape, device='xpu', dtype=dtype)
+        rand = lambda shape, dtype: torch.randint(low=-127, high=128, size=shape, device='xpu', dtype=dtype)
     if B == 1:
         a = rand((M, K), dtype)
         b = rand((K, N), dtype)
@@ -281,12 +282,15 @@ def benchmark(B, M, N, K, dtype, provider):
             c = torch.empty((M, N), device='xpu', dtype=res_dtype)
             kernel_name = 'matmul_kernel_with_block_pointers'
         triton_fn = lambda: matmul(a, b, d, c)
-        # Torch does not support integer calculation in matmul 
+        # Torch does not support integer calculation in matmul
         torch_device = 'xpu' if dtype.is_floating_point else 'cpu'
         torch_dtype = dtype if dtype.is_floating_point else res_dtype
-        torch_fn = lambda: torch.matmul(a.to(device=torch_device, dtype=torch_dtype), b.to(device=torch_device, dtype=torch_dtype)).to(device='xpu', dtype=res_dtype) + d
+        torch_fn = lambda: torch.matmul(a.to(device=torch_device, dtype=torch_dtype),
+                                        b.to(device=torch_device, dtype=torch_dtype)).to(device='xpu', dtype=res_dtype
+                                                                                         ) + d
         rtol = 1e-2 if a.dtype == torch.bfloat16 else 1e-3
-        if dtype.is_floating_point or [B, M, N, K] in [[1, 1024, 1024, 1024], [1, 2048, 2048, 2048], [1, 512, 8192, 32768], [4, 32768, 4096, 128]]:
+        if dtype.is_floating_point or [B, M, N, K] in [[1, 1024, 1024, 1024], [1, 2048, 2048, 2048],
+                                                       [1, 512, 8192, 32768], [4, 32768, 4096, 128]]:
             # torch int8 matmul on GPU is not supported. only check a few int8 shapes to reduce runtime
             benchmark_suit.assert_close(triton_fn(), torch_fn(), atol=1e-4, rtol=rtol, err_msg='triton to torch')
         _, min_ms, max_ms, mean_ms, cv = benchmark_suit.do_bench(triton_fn, n_warmup=10, n_repeat=10,
